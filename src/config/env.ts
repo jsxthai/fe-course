7ab5ad7a34818e31b2e5f8ee2532ebe5e7cd1cf3
unknown <jsxthai@gmail.com>
2021-02-26 16:49:00 +0700

    hotfix edit env servre online

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,1 +1,2 @@
-export const apolloServerUri = process.env.REACT_APP_APOLLO_SERVER_URI;
+export const apolloServerUri =
+  process.env.REACT_APP_APOLLO_SERVER_URI || "http://localhost:4000/graphql";
--- src/setup/apolloClient.ts
+++ src/setup/apolloClient.ts
@@ -1,13 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-// import { apolloServerUri } from "./../config/env";
+import { apolloServerUri } from "./../config/env";
 
-// local : http://localhost:4000/graphql
 const client = new ApolloClient({
-  // uri: apolloServerUri,
-  uri: "http://localhost:4000/graphql",
-
+  uri: apolloServerUri,
   cache: new InMemoryCache(),
 });
 
-// console.log(apolloServerUri);
 export default client;
